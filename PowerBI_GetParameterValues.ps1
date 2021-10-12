f7c96cb70de93942a9e5d19821f4226c50caf85c
Marc Lelijveld <marc.lelijveld@me.com>
2021-10-12 14:14:40 +0200

    Adding two new code snippets
    New code snippets to:
    - Get refresh schedule
    - Get parameter values

---
+++ PowerBI_GetParameterValues.ps1
@@ -0,0 +1,25 @@
+﻿<# 
+This script helps you to get insights in the current parameter values for a Power BI dataset.
+If you are not the owner of the dataset, you will not be able to see the parameter values in the Power BI Service. 
+Use this code snippet instead to get insights!  
+#>
+
+# =================================================================================================================================================
+# General parameters
+# =================================================================================================================================================
+# Specify Power BI parameters for workspace and dataset where the current dataset is stored
+$WorkspaceID = ""
+$DatasetID = ""
+
+# =================================================================================================================================================
+# Task execution
+# =================================================================================================================================================
+
+# Connect to Power BI Service using OAuth
+Connect-PowerBIServiceAccount
+
+# Get parameter values
+$GetParameterValues = Invoke-PowerBIRestMethod -Method GET -Url "groups/$WorkspaceID/datasets/$DatasetID/parameters" | ConvertFrom-Json
+
+# Display parameter values and settings on screen
+$GetParameterValues.Value
---
+++ PowerBI_GetRefreshSchedule.ps1
@@ -0,0 +1,25 @@
+﻿<# 
+This script helps you to get insights in the current refresh schedule for a Power BI dataset.
+If you are not the owner of the dataset, you will not be able to see the refresh schedule configured in the Power BI Service. 
+Use this code snippet instead to get insights!  
+#>
+
+# =================================================================================================================================================
+# General parameters
+# =================================================================================================================================================
+# Specify Power BI parameters for workspace and dataset where the current dataset is stored
+$WorkspaceID = ""
+$DatasetID = ""
+
+# =================================================================================================================================================
+# Task execution
+# =================================================================================================================================================
+
+# Connect to Power BI Service using OAuth
+Connect-PowerBIServiceAccount
+
+# Get refreshschedule values
+$GetRefreshSchedule = Invoke-PowerBIRestMethod -Method GET -Url "groups/$WorkspaceID/datasets/$DatasetID/refreshSchedule" | ConvertFrom-Json
+
+# Display Refresh Schedule on screen
+$GetRefreshSchedule
