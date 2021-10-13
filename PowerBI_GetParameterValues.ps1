a1dd71fcd6cfddb593344750f1370b16b495cb5c
Marc Lelijveld <marc.lelijveld@me.com>
2021-10-13 15:05:32 +0200

    Update script with link to blog post

--- PowerBI_GetParameterValues.ps1
+++ PowerBI_GetParameterValues.ps1
@@ -2,6 +2,9 @@
 This script helps you to get insights in the current parameter values for a Power BI dataset.
 If you are not the owner of the dataset, you will not be able to see the parameter values in the Power BI Service. 
 Use this code snippet instead to get insights!  
+
+More details about this script and the use case behind it, please read this blog post: 
+https://data-marc.com/2021/10/13/insights-in-power-bi-dataset-parameters-and-refresh-schedule/
 #>
 
 # =================================================================================================================================================
--- PowerBI_GetRefreshSchedule.ps1
+++ PowerBI_GetRefreshSchedule.ps1
@@ -1,7 +1,10 @@
 ﻿<# 
 This script helps you to get insights in the current refresh schedule for a Power BI dataset.
 If you are not the owner of the dataset, you will not be able to see the refresh schedule configured in the Power BI Service. 
-Use this code snippet instead to get insights!  
+Use this code snippet instead to get insights!
+
+More details about this script and the use case behind it, please read this blog post: 
+https://data-marc.com/2021/10/13/insights-in-power-bi-dataset-parameters-and-refresh-schedule/
 #>
 
 # =================================================================================================================================================
