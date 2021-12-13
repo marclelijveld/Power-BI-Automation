15605c6f9a4c513d7a5cb42b2a806993385aeeec
Marc Lelijveld <marc.lelijveld@me.com>
2021-12-13 20:43:02 +0100

    Updated script + included link to blog

--- PowerBI_RebindReportToDataset.ps1
+++ PowerBI_RebindReportToDataset.ps1
@@ -1,14 +1,21 @@
-﻿<#
+﻿# =================================================================================================================================================
+# Documentation
+# =================================================================================================================================================
+<#
 This script helps you to rebind an existing already published report to another dataset in the Power BI Service. 
-Please specify the parameters below and make sure that the account has access to the workspaces where the datast and report are located. 
+Please specify the parameters below and make sure that the account has access to the workspaces where the dataset and report are located. 
 
 In case of a service principal or app registration, make sure that the required scope and persmissons is matched: 
 Scope: Report.ReadWrite.All
 Permission: Report - Write permissions. Target dataset - Build permissions
 
-For full documenation, please look at: https://docs.microsoft.com/en-us/rest/api/power-bi/reports/rebindreportingroup?WT.mc_id=DP-MVP-5003435
+For full documenation on this API, please look at: https://docs.microsoft.com/en-us/rest/api/power-bi/reports/rebindreportingroup?WT.mc_id=DP-MVP-5003435
+For use cases to leverage below script, have a look at: https://data-marc.com/2021/12/13/unable-to-open-or-download-power-bi-report-because-link-to-azure-analysis-services-is-gone-what-now/
 #> 
 
+# =================================================================================================================================================
+# General parameters
+# =================================================================================================================================================
 # Run parameters, please specify below parameters
 $WorkspaceId = "{Your workspace id}"
 $ReportId = "{Report id to rebind}"
@@ -17,6 +24,9 @@ $TargetDatasetId = "{Dataset Id where you want the report to bind to}"
 # Base variables
 $BasePowerBIRestApi = "https://api.powerbi.com/v1.0/myorg/"
 
+# =================================================================================================================================================
+# Check task for Power BI Module
+# =================================================================================================================================================
 # Check whether the Power bI module is installed. If not, it will be installed. 
 $moduleName = Get-Module -ListAvailable -Verbose:$false | Where-Object { $_.Name -eq "MicrosoftPowerBIMgmt" } | Select-Object -ExpandProperty Name;
 if ([string]::IsNullOrEmpty($moduleName)) {
@@ -26,6 +36,9 @@ if ([string]::IsNullOrEmpty($moduleName)) {
     Write-Host -ForegroundColor White "==============================================================================";
 }
 
+# =================================================================================================================================================
+# Task execution
+# =================================================================================================================================================
 # Connect to Power BI service Account
 Write-Host -ForegroundColor White "Connect to PowerBI service"
 Connect-PowerBIServiceAccount
