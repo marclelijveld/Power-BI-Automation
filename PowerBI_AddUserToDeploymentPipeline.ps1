830ce62f421bc961d7adb9e8cad4d3f72e1c3624
Marc Lelijveld <Marc.lelijveld@outlook.com>
2024-05-13 16:23:45 +0200

    Find and provide access to orphaned pipelines

---
+++ PowerBI_AddUserToDeploymentPipeline.ps1
@@ -0,0 +1,26 @@
+﻿# Sign in with Fabric / Power BI Admin account
+Connect-PowerBIServiceAccount
+
+# List deployment pipelines + stages
+$AllPipelines = Invoke-PowerBIRestMethod -Method GET -Url "https://api.powerbi.com/v1.0/myorg/admin/pipelines?$expand=stages"
+$pipelines = $AllPipelines | ConvertFrom-Json
+
+# Filter list on specific keyword
+$pipelines.value | where {$_ -match 'cash'} #replace the filter value if you're looking for a pipeline with a certain name
+
+# Investigate current users of a certain pipeline
+$PipelineId = "{id}" #Fill in the pipeline id from the response of above API
+Invoke-PowerBIRestMethod -Method GET -Url "https://api.powerbi.com/v1.0/myorg/admin/pipelines/$PipelineId/users"
+
+# Update users of a certain pipeline
+$UPN = "{UPN}" #Fill in the user principal name of the user you want to add to the pipeline
+
+$body = @"
+{
+  "identifier": "$UPN",
+  "accessRight": "Admin",
+  "principalType": "User"
+}
+"@
+
+Invoke-PowerBIRestMethod -Method POST -Body $body -Url "https://api.powerbi.com/v1.0/myorg/admin/pipelines/$PipelineId/users"
