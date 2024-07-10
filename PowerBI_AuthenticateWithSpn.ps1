d7206bdc64c356c11acf7efc0069861155176cd8
Marc Lelijveld <Marc.lelijveld@outlook.com>
2024-07-10 10:45:41 +0200

    Authentication sample

---
+++ PowerBI_AuthenticateWithSpn.ps1
@@ -0,0 +1,17 @@
+﻿# =================================================================================================================================================
+# Check if Power BI Module is available, if not install
+# =================================================================================================================================================
+$moduleName = Get-Module -ListAvailable -Verbose:$false | Where-Object { $_.Name -eq "MicrosoftPowerBIMgmt" } | Select-Object -ExpandProperty Name;
+if ([string]::IsNullOrEmpty($moduleName)) {
+    Write-Host -ForegroundColor White "==============================================================================";
+    Write-Host -ForegroundColor White  "Install module MicrosoftPowerBIMgmt...";
+    Install-Module MicrosoftPowerBIMgmt -SkipPublisherCheck -AllowClobber -Force
+    Write-Host -ForegroundColor White "==============================================================================";
+}
+
+# Define variables
+$TenantId = "{YourTenantId}"
+
+# Connect using SPN
+Connect-PowerBIServiceAccount -ServicePrincipal -Credential (Get-Credential) -Tenant $TenantId
+$ClientId = $SpInlog.ClientId
